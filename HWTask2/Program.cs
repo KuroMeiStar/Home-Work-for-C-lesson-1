df070316ba83c41cd196ecf7892bd2944f6eeeac
Bekzhan <mnf.kuro@mail.ru>
2023-01-27 00:20:47 +0500

    Added homeworks for lesson 1

---
+++ HWTask2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3;
+
+int a, b;
+Console.WriteLine("Enter the first number:");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+b = int.Parse(Console.ReadLine());
+
+if(a > b) {
+     Console.WriteLine("The larger number is: " + a);
+     Console.WriteLine("The smaller number is: " + b);
+} else {
+     Console.WriteLine("The larger number is: " + b);
+     Console.WriteLine("The smaller number is: " + a);
+  
+}
+
---
+++ HWTask4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+
+ int num1, num2, num3;
+        Console.WriteLine("Enter the first number:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the second number:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the third number:");
+        num3 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Math.Max(num1, Math.Max(num2, num3));
+        Console.WriteLine("The maximum number is: " + result);;
