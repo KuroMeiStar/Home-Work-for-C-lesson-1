874f2c1b975ff8478938e6ff1ff32a4353bdcd98
Bekzhan <mnf.kuro@mail.ru>
2023-02-03 21:11:46 +0500

    Added hometasks for lesson 2 part 3

---
+++ HWTask19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да;
+
+ Console.WriteLine("Enter a 5-digit number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int a = num / 10000;
+            int b = (num / 1000) % 10;
+            int c = (num / 100) % 10;
+            int d = (num / 10) % 10;
+            int e = num % 10;
+
+            if (a == e && b == d)
+            {
+                Console.WriteLine(num + " is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine(num + " is not a palindrome");
+            }
+
+            Console.ReadKey();
---
+++ HWTask21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53;
+
+Console.WriteLine("Enter the X, Y and Z coordinates of the first point: ");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double z1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the X, Y and Z coordinates of the second point: ");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double z2 = double.Parse(Console.ReadLine());
+
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+            Console.WriteLine("The distance between the two points is: " + distance);
