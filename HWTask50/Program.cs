a981cf657eec7b90001b21607b4b0267f66bd18a
KuroMeiStar <122556646+KuroMeiStar@users.noreply.github.com>
2023-02-17 23:05:08 +0500

    Added HW tasks 47, 50 and 52

---
+++ HWTask50/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет;
+
+int[,] array = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+
+int rows = array.GetLength(0);
+int cols = array.GetLength(1);
+
+Console.Write("Enter the row position: ");
+int rowPos = int.Parse(Console.ReadLine());
+Console.Write("Enter the column position: ");
+int colPos = int.Parse(Console.ReadLine());
+
+ if (rowPos < 0 || rowPos >= rows || colPos < 0 || colPos >= cols)
+  {
+   Console.WriteLine("There is no such element in the array.");
+  }
+ else
+   {
+    int element = array[rowPos, colPos];
+    Console.WriteLine($"The value of the element at position ({rowPos}, {colPos}) is {element}.");
+   }
---
+++ HWTask52/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.;
+
+
+
+int[,] array = new int[,] { {1, 4, 7, 2},{5, 9, 2, 3},{8, 4, 2, 4}};
+int numRows = array.GetLength(0);
+int numCols = array.GetLength(1);
+double[] columnMeans = new double[numCols];
+
+ for (int j = 0; j < numCols; j++) 
+  {
+   double sum = 0;
+ for (int i = 0; i < numRows; i++) 
+   {
+    sum += array[i,j];
+   }
+   columnMeans[j] = sum / numRows;
+  }
+
+ for (int j = 0; j < numCols; j++) 
+    {
+     Console.WriteLine("Arithmetic mean of column {0}: {1}", j+1, columnMeans[j]);
+    }
