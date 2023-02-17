9a7281c39c18f38be5f109603826222b8f5dc9dd
KuroMeiStar <122556646+KuroMeiStar@users.noreply.github.com>
2023-02-17 23:02:16 +0500

    Added HW tasks 41 and 43

---
+++ HWTask41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3;
+
+Console.Write("Enter the value of M: ");
+int M = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+ for (int i = 0; i < M; i++)
+   {
+     Console.Write("Enter a number: ");
+     int num = int.Parse(Console.ReadLine());
+
+      if (num > 0)
+        {
+         count++;
+        }
+   }
+
+Console.WriteLine("You entered {0} numbers greater than 0.", count);
+
+Console.ReadKey();
