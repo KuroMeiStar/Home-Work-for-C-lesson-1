6a8051ac5280e3f2c7ddf10f277e272433e0a0ae
KuroMeiStar <122556646+KuroMeiStar@users.noreply.github.com>
2023-02-24 16:52:33 +0500

    Added HW tasks 64,66,68

---
+++ HWTask64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1";
+
+int N = 8;
+
+PrintNumbers(N);
+
+
+void PrintNumbers(int n)
+ {
+  if (n == 0) // Base case
+   {
+    return;
+   }
+  else
+   {
+    Console.Write(n + " "); // Print current number
+    PrintNumbers(n - 1); // Recursive call to print the next number
+   }
+ }
+
---
+++ HWTask66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30;
+
+
+int M = 4, N = 8;
+int sum = 0;
+
+for (int i = M; i <= N; i++)
+ {
+  sum += i;
+ }
+
+Console.WriteLine("The sum of natural numbers from {0} to {1} is {2}", M, N, sum);
